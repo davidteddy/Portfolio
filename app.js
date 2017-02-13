b54d50765da2ff6a7a5b60d2606c26bff6780afb
David Teddy <davidteddy@outlook.com>
2017-02-13 11:27:26 -0800

    adding handle bars and getting stuff ready for #4

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict';
-function projects(name, shortDescrip, link, dateCompleted){
+var myWorks = [];
+
+function Projects(name, shortDescrip, link, dateCompleted){
   this.name = name;
   this.shortDescrip = shortDescrip;
   this.link = link;
   this.dateCompleted = dateCompleted;
 }
+
+Projects.prototype.toHtml = function(){
+  var source = $('#projects-templates').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+}
+
+projectData.forEach(function(projectObject){
+  myWorks.push(new Projects(projectObject));
+});
+
+myWorks.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
