1b03d2a9a3d4ef57b096b28d5bde5aef6f66e0da
David Teddy <davidteddy@outlook.com>
2017-02-20 09:35:36 -0800

    got the stuff to show up

--- public/app.js
+++ public/app.js
@@ -16,21 +16,37 @@ Projects.prototype.toHtml = function(){
   return templateRender(this);
 }
 
-projectData.forEach(function(projectObject){
-  myWorks.push(new Projects(projectObject));
-});
-
-myWorks.forEach(function(a){
-  $('#projects').append(a.toHtml());
-});
-
+Projects.loadall = function(projectData){
+    projectData.forEach(function(projectObject){
+      myWorks.push(new Projects(projectObject));
+    });
+};
+myWorks.initIndexPage = function(){
+  myWorks.forEach(function(a){
+    $('#projects').append(a.toHtml());
+  });
+};
 myWorks.handleMainNav = function() {
   $('.nav-bar').on('click', '.tab', function() {
     $('.tab-content').hide();
     $('#' + $(this).data('content')).fadeIn();
   });
+};
+Projects.fetchAll = function(){
+  if (localStorage.myWorks){
+    Projects.loadall(JSON.parse(localStorage.getItem('myWorks')));
+    myWorks.initIndexPage();
+  } else {
+    $.getJSON('/public/bio.json')
+    .then(function(data){
+      localStorage.setItem('myWorks', JSON.stringify(data));
+      Projects.loadall(data);
+      myWorks.initIndexPage();
+    });
+  }
+}
 
 //  $('.nav-bar .tab:first').click();
-};
+
 
 myWorks.handleMainNav();
