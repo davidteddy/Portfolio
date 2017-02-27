e9b9fc1e1d0e156e8e2e88cd9c29b6755f46e04a
David Teddy <davidteddy@outlook.com>
2017-02-26 19:26:20 -0800

    getting ready to deploy

--- public/githubToken.js
+++ public/githubToken.js
@@ -1,1 +1,1 @@
-const gitHubToken = '737ef36688c64aa54ea91c7ae80a70af2642008b';
+// const gitHubToken = '737ef36688c64aa54ea91c7ae80a70af2642008b';
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+
+const pg = require('pg');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
+const conString = 'postgres://postgres:1234@localhost:5432/postgres';
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
 
 app.get('/', function(request, response){
   response.sendFile('index.html', {root: './public'});
@@ -15,6 +25,8 @@ app.get('/about', function(request, response){
 app.get('/repos', function(request, response){
   response.sendFile('index.html', {root: './public'});
 });
+
+app.get('/github/*', proxyGitHub);
 
 app.listen(PORT, function(){
   console.log('Port is', PORT);
