a8f98866a73527bd445fa0d0a7452649eb62c4fb
David Teddy <davidteddy@outlook.com>
2017-02-28 08:40:25 -0800

    adding the rest that is needed

--- app.js
+++ app.js
@@ -5,3 +5,39 @@ function projects(name, shortDescrip, link, dateCompleted){
   this.link = link;
   this.dateCompleted = dateCompleted;
 }
+
+var myWorks = [];
+
+function Projects(opt){
+  this.name = opt.name;
+  this.shortDescrip = opt.shortDescrip;
+  this.link = opt.link;
+  this.dateCompleted = opt.dateCompleted;
+}
+
+
+
+Projects.prototype.toHtml = function(){
+  var source = $('#projects-templates').html();
+  var templateRender = Handlebars.compile(source);
+  return templateRender(this);
+}
+
+projectData.forEach(function(projectObject){
+  myWorks.push(new Projects(projectObject));
+});
+
+myWorks.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
+
+myWorks.handleMainNav = function() {
+  $('.nav-bar').on('click', '.tab', function() {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+
+//  $('.nav-bar .tab:first').click();
+};
+
+myWorks.handleMainNav();
