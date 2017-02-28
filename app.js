9637155f42e8a047371292b26b5ea3770759534d
David Teddy <davidteddy@outlook.com>
2017-02-28 08:53:27 -0800

    trying to get it to work

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-'use strict';
-function projects(name, shortDescrip, link, dateCompleted){
-  this.name = name;
-  this.shortDescrip = shortDescrip;
-  this.link = link;
-  this.dateCompleted = dateCompleted;
+var projects = [];
+
+function Project (opts) {
+  this.title = opts.title;
+  this.createdOn = opts.createdOn;
+  this.completedOn = opts.completedOn;
+  this.shortDescription = opts.shortDescription;
+  this.longDescription = opts.longDescription;
 }
 
-var myWorks = [];
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
 
-function Projects(opt){
-  this.name = opt.name;
-  this.shortDescrip = opt.shortDescrip;
-  this.link = opt.link;
-  this.dateCompleted = opt.dateCompleted;
-}
+  if (!this.createdOn) $newProject.addClass('draft');
+  $newProject.data('category', this.category);
 
+  $newProject.find('h1').html(this.title);
+  $newProject.find('.project-body').html(this.longDescription);
+  $newProject.find('time').attr('datetime', this.createdOn);
 
-
-Projects.prototype.toHtml = function(){
-  var source = $('#projects-templates').html();
-  var templateRender = Handlebars.compile(source);
-  return templateRender(this);
-}
-
-projectData.forEach(function(projectObject){
-  myWorks.push(new Projects(projectObject));
-});
-
-myWorks.forEach(function(a){
-  $('#projects').append(a.toHtml());
-});
-
-myWorks.handleMainNav = function() {
-  $('.nav-bar').on('click', '.tab', function() {
-    $('.tab-content').hide();
-    $('#' + $(this).data('content')).fadeIn();
-  });
-
-//  $('.nav-bar .tab:first').click();
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000) + ' days ago');
+  $newProject.append('<hr>');
+  return $newProject;
 };
 
-myWorks.handleMainNav();
+projectData.sort(function(a,b) {
+  return (new Date(b.createdOn)) - (new Date(a.createdOn));
+});
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
+
+projects.forEach(function(a) {
+  $('#projects').append(a.toHtml());
+});
